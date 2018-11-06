db40ab7b6d91fccf3d933105e7963f41d969f83e
Tesfa Yohannes <tesfa@singularitynet.io>
2018-11-06 09:00:01 +0100

    Update for rqt

--- test_job_creator.py
+++ test_job_creator.py
@@ -6,17 +6,15 @@ import yaml
 
 
 job_addresses = []
-for i in range(1):
+for i in range(1000):
     p = subprocess.Popen(["snet", "agent", "--at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "create-jobs","--number", "1", "--max-price", "10000000", "--funded", "--signed", "-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     contract, err = p.communicate()
     try:
         job_address = yaml.load(contract)['jobs']
-        print(job_address[0]['job_address'])
+        # print(job_address[0]['job_address'])
         job_addresses.append(job_address[0]['job_address'])
     except TypeError:
         print("Failed to create a job")
-
-    time.sleep(1)
 
 with open('jobaddresses ' + time.ctime() + '.txt', 'w') as f:
     for job in job_addresses:
--- test_rpc_call.py
+++ test_rpc_call.py
@@ -1,6 +1,5 @@
 import jsonrpcclient
 import base64
-import cv2
 
 import grpc
 from service_spec.EmotionService_pb2_grpc import EmotionRecognitionStub
@@ -30,18 +29,51 @@ def test_deployed_service(job_address):
     p = subprocess.Popen(["snet", "--print-traceback", "client", "call", "--agent-at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "--job-at", job_address, "classify", "query2.json","-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     result, err = p.communicate()
+    print(result)
+    print(err)
 
     preprocessed_results = yaml.load(result)['response']
-
-    print(result)
     print(preprocessed_results)
+
+    return preprocessed_results
+
+def test_deployed_service(job_address):
+    query = '{"image_type": "png", "image" : "' + str(image_64) + '"}'
+    with open('query2.json', 'wt') as f:
+        f.write(str(query))
+
+    p = subprocess.Popen(["snet", "--print-traceback", "client", "call", "--agent-at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "classify", "query2.json","-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    result, err = p.communicate()
+    print(result)
     print(err)
+
+    preprocessed_results = yaml.load(result)['response']
+    print(preprocessed_results)
+
+    return preprocessed_results
+
+def test_call():
+    query = '{"image_type": "png", "image" : "' + str(image_64) + '"}'
+    with open('query2.json', 'wt') as f:
+        f.write(str(query))
+
+    p = subprocess.Popen(["snet", "client", "call", "--agent-at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "classify", "query2.json","-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    result, err = p.communicate()
+    print(result)
+    print(err)
+
+    preprocessed_results = yaml.load(result)['response']
+    print(preprocessed_results)
+
     return preprocessed_results
 
 parser = argparse.ArgumentParser(prog="test")
 
 parser.add_argument("--test_grpc", help="test grpc at given port")
 parser.add_argument("--test_with_job", help="given a job test the service for action")
+parser.add_argument("--test", help="given a job test the service for action")
 args = parser.parse_args(sys.argv[1:])
 
 if args.test_grpc is not None:
@@ -49,3 +81,6 @@ if args.test_grpc is not None:
 
 if args.test_with_job is not None:
     test_deployed_service(args.test_with_job)
+
+if args.test is not None:
+    test_call()
