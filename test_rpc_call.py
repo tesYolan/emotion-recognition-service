08fd7dd2382a698b0fe828344953619fcb052c8b
Tesfa Yohannes <tmyohannes@gmail.com>
2018-11-07 07:52:16 +0300

    Test mpe-client call_server calls

--- test_rpc_call.py
+++ test_rpc_call.py
@@ -26,7 +26,7 @@ def test_deployed_service(job_address):
     with open('query2.json', 'wt') as f:
         f.write(str(query))
 
-    p = subprocess.Popen(["snet", "--print-traceback", "client", "call", "--agent-at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "--job-at", job_address, "classify", "query2.json","-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(["snet", "--print-traceback", "client", "call", "--agent-at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "--job-at", job_address, "classify", query,"-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     result, err = p.communicate()
     print(result)
@@ -57,7 +57,6 @@ def test_call():
     query = '{"image_type": "png", "image" : "' + str(image_64) + '"}'
     with open('query2.json', 'wt') as f:
         f.write(str(query))
-
     p = subprocess.Popen(["snet", "client", "call", "--agent-at", "0x7fE17B093E13379247336DDD846deF8624Ae8a9C", "classify", "query2.json","-y"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     result, err = p.communicate()
@@ -68,6 +67,28 @@ def test_call():
     print(preprocessed_results)
 
     return preprocessed_results
+
+def test_mpe_call():
+    query = '{"image_type": "png", "image" : "' + str(image_64) + '"}'
+    with open('query2.json', 'wt') as f:
+        f.write(str(query))
+    p = subprocess.Popen(["snet", "mpe-client", "call_server", "0x38506005d6b25386aac998448ae5eb48f87f4277", "0","10","34.216.72.29:6205", "EmotionRecognition", "classify", "query2.json"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result, err = p.communicate()
+    print(result)
+    results = str(result)
+    print(err)
+
+    preprocessed_emotions = results[results.find('predictions'):results.find('bounding_boxes')].replace('predictions: ','').split("\\n")
+    print(preprocessed_emotions)
+    emotions = []
+    for emotion in preprocessed_emotions:
+        p_emotion = emotion.replace('"','')
+        if p_emotion != '':
+            emotions.append(p_emotion)
+
+    print(emotions)
+
+    return emotions
 
 parser = argparse.ArgumentParser(prog="test")
 
@@ -83,4 +104,4 @@ if args.test_with_job is not None:
     test_deployed_service(args.test_with_job)
 
 if args.test is not None:
-    test_call()
+    test_mpe_call()
