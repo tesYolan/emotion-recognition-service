d6c3c9e6e7f55e8cc82ccb98ed38f42de1fbd192
Tesfa Yohannes <tmyohannes@gmail.com>
2019-01-30 09:26:37 +0300

    Fix for misc issues with installations scripts.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 snet_daemon_v=0.1.5
 
 # apt install tar
-if [ ! -d snetd-$snet_daemon_v ] ; then
+if [ ! -d snet-daemon-v$snet_daemon_v ] ; then
 	echo "Downloading snetd-linux"
 	wget https://github.com/singnet/snet-daemon/releases/download/v$snet_daemon_v/snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
 	tar -xzf snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
