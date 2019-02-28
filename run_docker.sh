3271d8f447600ce2c6aa9f5619c86f3fe24ba57f
Tesfa Yohannes <tmyohannes@gmail.com>
2019-02-28 09:04:52 +0300

    Add mainnet net configuration and remove kovan from setup script

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -f Dockerfile.gpu -t singularitynet/emotion-recognition-service .
-docker run -v /etc/letsencrypt:/etc/letsencrypt --runtime=nvidia --name emotion-recognition-service -p 6305:6305 -p 6205:6205 -d -it singularitynet/emotion-recognition-service:latest
+docker run -v etcd:etcd -v /etc/letsencrypt:/etc/letsencrypt --runtime=nvidia --name emotion-recognition-service -p 6305:6305 -p 6405:6405 -d -it singularitynet/emotion-recognition-service:latest
