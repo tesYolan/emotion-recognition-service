0d6de3f2b4ed03a8ca0e457860fb54dea9c2c3fc
Tesfa Yohannes <tmyohannes@gmail.com>
2019-02-28 09:15:43 +0300

    Fix absolute path for etcd config

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -f Dockerfile.gpu -t singularitynet/emotion-recognition-service .
-docker run -v etcd:etcd -v /etc/letsencrypt:/etc/letsencrypt --runtime=nvidia --name emotion-recognition-service -p 6305:6305 -p 6405:6405 -d -it singularitynet/emotion-recognition-service:latest
+docker run -v /home/tesfa/emotion-recognition-service/etcd:/emotion-recognition-service/etcd -v /etc/letsencrypt:/etc/letsencrypt --runtime=nvidia --name emotion-recognition-service -p 6305:6305 -p 6405:6405 -d -it singularitynet/emotion-recognition-service:latest
